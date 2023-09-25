e877e893576a2d659f3edf5c49e90a2c670a0d24
BLACKHENZ29 <ayehenz29@gmail.com>
2023-09-25 08:53:48 +0100

    Tweaks

--- route/auth.route.js
+++ route/auth.route.js
@@ -2,7 +2,7 @@ import express from 'express';
 import multer from 'multer';
 import { isAdmin } from './../middleware/admin.js';
 import { loggedIn } from './../middleware/loginAccess.js';
-import { AdminLogIn, RegisterAdmin, updatePassword, uploadProfilePicture } from '../controller/auth.controller.js';
+import { AdminLogIn, RegisterAdmin, updatePassword, updateUser, uploadProfilePicture } from '../controller/auth.controller.js';
 import { profilePictureStorage }  from '../config/cloudinary.js';
 
 const adminRouter = express.Router();
@@ -15,6 +15,8 @@ adminRouter.get('/Login', AdminLogIn);
 adminRouter.put('/update-password', isAdmin, loggedIn, updatePassword);
 
 adminRouter.post('/profile-picture', profilePictureUpload.single('profileImage'), uploadProfilePicture);
+
+adminRouter.put('/update-profile', isAdmin, loggedIn, updateUser);
 
 
 
