b566bb8e8318b05ad4b029d5401407d1d0afcb56
BLACKHENZ29 <ayehenz29@gmail.com>
2023-11-15 08:07:36 +0000

    Remastered

--- controller/expense.controller.js
+++ controller/expense.controller.js
@@ -10,33 +10,42 @@ export const createExpense = async (req, res) => {
   }
   try {
     const { title, totalAmount, amountPaid, description, category, datePaid } = req.body;
-    if (totalAmount < 0 || amountPaid < 0) {
+    const totalAmountInt = parseInt(totalAmount);
+    const amountPaidInt = parseInt(amountPaid);
+
+    if (totalAmountInt < 0 || amountPaidInt < 0) {
       return res.status(400).json({ message: 'Please enter accurate numbers.' });
     }
+
     let paymentStatus;
-    if (amountPaid < totalAmount) {
+    if (amountPaidInt < totalAmountInt) {
       paymentStatus = 'Partially Paid';
-    } else if (amountPaid === totalAmount) {
+    } else if (amountPaidInt === totalAmountInt) {
       paymentStatus = 'Fully Paid';
     } else {
       return res.status(400).json({ message: 'The amount paid is greater than the total amount.' });
     }
+
     const existingExpense = await ExpenseMod.findOne({ title, paymentStatus: 'Fully Paid', datePaid });
     if (existingExpense) {
       return res.status(400).json({ message: 'Expense already marked as fully paid on the same date.' });
     }
-    const balance = totalAmount - amountPaid;
+
+    const balance = totalAmountInt - amountPaidInt;
+
     const newExpense = new ExpenseMod({
       title,
-      totalAmount,
-      amountPaid,
+      totalAmount: totalAmountInt,
+      amountPaid: amountPaidInt,
       description,
       category,
       paymentStatus,
       balance,
       datePaid,
     });
+
     await newExpense.save();
+
     const employee = await EmployeeMod.findById(userId).populate('superAdminId');
     if (employee) {
       const superAdmin = await AdminModel.findById(employee.superAdminId);
@@ -63,28 +72,48 @@ export const createExpense = async (req, res) => {
 
 
 
+
 export const getAllExpenses = async (req, res) => {
   const userId = req.userAuth;
   if (!userId) {
     return res.status(401).json({ error: 'Unauthorized' });
   }
   try {
+    const currentDate = new Date();
+    const startOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+    const endOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
     let expenses;
+    let totalAmountPaidForCurrentMonth = 0;
     const employee = await EmployeeMod.findById(userId).populate('superAdminId');
     if (employee) {
       const superAdmin = await AdminModel.findById(employee.superAdminId);
       if (superAdmin) {
-        expenses = await ExpenseMod.find({ _id: { $in: superAdmin.expenses } });
+        expenses = await ExpenseMod.find({
+          _id: { $in: superAdmin.expenses },
+          datePaid: { $gte: startOfMonth, $lte: endOfMonth },
+        });
+        totalAmountPaidForCurrentMonth = expenses.reduce(
+          (total, expense) => total + (expense.amountPaid || 0),
+          0
+        );
       }
     } else {
       const superAdmin = await AdminModel.findById(userId);
       if (superAdmin) {
-        expenses = await ExpenseMod.find({ _id: { $in: superAdmin.expenses } });
+        expenses = await ExpenseMod.find({
+          _id: { $in: superAdmin.expenses },
+          datePaid: { $gte: startOfMonth, $lte: endOfMonth },
+        });
+        totalAmountPaidForCurrentMonth = expenses.reduce(
+          (total, expense) => total + (expense.amountPaid || 0),
+          0
+        );
       } else {
         return res.status(404).json({ message: 'User not found.' });
       }
     }
-    return res.status(200).json(expenses);
+
+    return res.status(200).json({ expenses, totalAmountPaidForCurrentMonth });
   } catch (error) {
     console.error('Error:', error);
     return res.status(500).json({ message: 'Internal Server Error' });
@@ -118,7 +147,7 @@ export const updateExpense = async (req, res) => {
     const updatedExpense = await ExpenseMod.findByIdAndUpdate(
       expenseId,
       {
-        amountPaid: expense.amountPaid + amountPaid,
+        amountPaid: Number(expense.amountPaid) + Number(amountPaid),
         balance: newBalance,
         paymentStatus,
       },
